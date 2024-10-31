9d62317d68139658f174299cbc9aa537574fbeea
bannednull <bannednull@gmail.com>
2024-10-31 13:24:12 -0300

    header

---
+++ src/app/_components/home-header.tsx
@@ -0,0 +1,34 @@
+import { ToggleTheme } from '@/components/toggle-theme';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '@/components/ui/navigation-menu';
+import Link from 'next/link';
+
+function HomeHeader() {
+  return (
+    <header className="py-4">
+      <nav className="mx-auto flex max-w-screen-lg items-center justify-between">
+        <h1 className="text-xl font-bold">Bakan</h1>
+
+        <NavigationMenu className="mx-auto">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className="text-muted-foreground hover:text-foreground">
+                  Home
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        <ToggleTheme />
+      </nav>
+    </header>
+  );
+}
+
+export default HomeHeader;
