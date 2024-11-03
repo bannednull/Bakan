e00e13c875cec065e7031d2dc9805f74bdb3e454
bannednull <bannednull@gmail.com>
2024-11-02 22:06:17 -0300

    fill color

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,13 +8,13 @@ const Logo = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <rect width="40" height="40" fill="white" />
-    <rect x="45" width="120" height="40" fill="white" />
-    <rect x="122" y="37" width="43" height="83" fill="white" />
-    <rect x="162" y="80" width="40" height="82.5" fill="white" />
-    <rect y="80" width="125" height="40" fill="white" />
-    <rect x="40" y="164.5" width="122" height="40" fill="white" />
-    <rect y="45" width="40" height="118" fill="white" />
+    <rect width="40" height="40" style={{ fill: 'hsl(var(--foreground))' }} />
+    <rect x="45" width="120" height="40" style={{ fill: 'hsl(var(--foreground))' }} />
+    <rect x="122" y="37" width="43" height="83" style={{ fill: 'hsl(var(--foreground))' }} />
+    <rect x="162" y="80" width="40" height="82.5" style={{ fill: 'hsl(var(--foreground))' }} />
+    <rect y="80" width="125" height="40" style={{ fill: 'hsl(var(--foreground))' }} />
+    <rect x="40" y="164.5" width="122" height="40" style={{ fill: 'hsl(var(--foreground))' }} />
+    <rect y="45" width="40" height="118" style={{ fill: 'hsl(var(--foreground))' }} />
   </svg>
 );
 export default Logo;
