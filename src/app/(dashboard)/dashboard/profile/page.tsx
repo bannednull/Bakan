4033307f145c3d3db3b57ad526902366a56918a7
bannednull <bannednull@gmail.com>
2024-11-05 14:39:35 -0300

    tabs billing

--- src/app/(dashboard)/dashboard/profile/page.tsx
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -18,6 +18,12 @@ function ProfilePage() {
                 Account
               </TabsTrigger>
               <TabsTrigger
+                value="billing"
+                className="w-full justify-start data-[state=active]:bg-accent"
+              >
+                Billing
+              </TabsTrigger>
+              <TabsTrigger
                 value="password"
                 className="w-full justify-start data-[state=active]:bg-accent"
               >
@@ -27,6 +33,9 @@ function ProfilePage() {
             <div className="flex-1 px-6 py-2">
               <TabsContent value="account" className="h-full">
                 <h1>Account</h1>
+              </TabsContent>
+              <TabsContent value="billing" className="h-full">
+                <h1>Billing</h1>
               </TabsContent>
               <TabsContent value="password" className="h-full">
                 <h1>Change password</h1>
