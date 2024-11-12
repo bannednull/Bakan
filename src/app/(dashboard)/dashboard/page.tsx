14c65de3e0b27a5465ffa0e3b0d18c40c99046b5
bannednull <bannednull@gmail.com>
2024-11-12 20:18:41 -0300

    dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -3,7 +3,10 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import Heading from '@dashboard/_components/heading';
 import type { Metadata } from 'next';
 import { prisma } from '@/lib/prisma';
-import { CreditCard, Users2 } from 'lucide-react';
+import { Book, CreditCard, Play, Users2 } from 'lucide-react';
+import Github from '@/components/brands/github';
+import Discord from '@/components/brands/discord';
+import X from '@/components/brands/x';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -41,6 +44,59 @@ async function DashboardPage() {
               <span>Total Users</span>
             </div>
             {usersCount}
+          </div>
+        </div>
+
+        <div className="my-3 grid grid-cols-[250px_1fr] border-y py-4">
+          <p className="text-muted-foreground">Your API KEY</p>
+        </div>
+
+        <div className="mt-4 grid grid-cols-3 gap-3">
+          <div className="col-span-2 space-y-3">
+            <div className="flex items-center gap-3 rounded-md border bg-accent/40 p-5">
+              <div className="flex h-12 w-12 items-center justify-center rounded-md bg-background">
+                <div className="rounded-md bg-sky-700 p-2">
+                  <Book size={16} color="white" />
+                </div>
+              </div>
+              <p className="text-xs text-muted-foreground">
+                <strong className="block text-base text-foreground">Read the documentation</strong>
+                Discover how you can make the most of it.
+              </p>
+            </div>
+
+            <div className="flex items-center gap-3 rounded-md border bg-accent/40 p-5">
+              <div className="flex h-12 w-12 items-center justify-center rounded-md bg-background">
+                <div className="rounded-md bg-emerald-700 p-2">
+                  <Play size={16} color="white" />
+                </div>
+              </div>
+              <p className="text-xs text-muted-foreground">
+                <strong className="block text-base text-foreground">Tutorial</strong>
+                Discover the concepts, reference, guides and tutorials.
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <div className="rounded-md border bg-accent/40 p-5">
+              <strong>Join the community</strong>
+              <p className="text-xs text-muted-foreground">
+                Discuss with team members, contributors and developers.
+              </p>
+
+              <div className="mt-3 grid grid-cols-2 gap-2">
+                <a href="#" className="flex items-center gap-2">
+                  <X /> X
+                </a>
+                <a href="#" className="flex items-center gap-2">
+                  <Github className="size-5" /> Github
+                </a>
+                <a href="#" className="flex items-center gap-2">
+                  <Discord className="size-5" /> Discord
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
